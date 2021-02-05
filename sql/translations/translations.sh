094eb0ca736c13918bc75901a5fa1803bc5c7436
wangjueming <wangjueming@ule.com>
2021-02-05 16:59:52 +0800

    add import BroadcastTextLocales.sql

--- sql/translations/translations.sh
+++ sql/translations/translations.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eo pipefail
 
 CONFIG_FILE="translations.config"
 DB_HOST="localhost"
@@ -16,24 +17,60 @@ fi
 
 . ${CONFIG_FILE}
 
+cd /tmp
+
+if [ -f "BroadcastTextLocales.sql" ]; then
+  rm BroadcastTextLocales.sql
+fi
 if [ "${CMANGOS_CORE}" = "classic" ]; then
+  echo "classic does not supported BroadcastTextLocales."
   CMANGOS_CORE=zero
+#https://raw.githubusercontent.com/cmangos/mangos-classic/master/sql/base/dbc/original_data/locales/BroadcastTextLocales.sql
 elif [ "${CMANGOS_CORE}" = "tbc" ]; then
+  wget --no-check-certificate https://raw.githubusercontent.com/cmangos/mangos-tbc/master/sql/base/dbc/original_data/locales/BroadcastTextLocales.sql
   CMANGOS_CORE=one
 elif [ "${CMANGOS_CORE}" = "wotlk" ]; then
+  wget --no-check-certificate https://raw.githubusercontent.com/cmangos/mangos-wotlk/master/sql/base/dbc/original_data/locales/BroadcastTextLocales.sql
   CMANGOS_CORE=two
 fi
 
-cd /tmp
+export MYSQL_PWD="${PASSWORD}"
+MYSQL_COMMAND="${MYSQL} -h${DB_HOST} -P${DB_PORT} -u${USERNAME} ${DATABASE}"
+#TODO 验证用户名密码
+
+LOCALE=""
+if [ "${TRANSLATIONS}" = "German" ]; then
+  LOCALE="deDE"
+elif [ "${TRANSLATIONS}" = "Spanish" ]; then
+  LOCALE="esES"
+elif [ "${TRANSLATIONS}" = "Spanish_South_American" ]; then
+  LOCALE="esMX"
+elif [ "${TRANSLATIONS}" = "French" ]; then
+  LOCALE="frFR"
+elif [ "${TRANSLATIONS}" = "Korean" ]; then
+  LOCALE="koKR"
+elif [ "${TRANSLATIONS}" = "Russian" ]; then
+  LOCALE="ruRU"
+elif [ "${TRANSLATIONS}" = "Taiwanese" ]; then
+  LOCALE="zhTW"
+elif [ "${TRANSLATIONS}" = "Italian" ]; then
+  LOCALE="itIT"
+else
+  LOCALE="zhCN"
+
+fi
+echo "> Processing BroadcastTextLocales.sql."
+cp -f BroadcastTextLocales.sql BroadcastTextLocales_bak.sql
+sed -i "s/),(/);\nINSERT INTO \`broadcast_text_locale\` VALUES (/g" BroadcastTextLocales_bak.sql
+cat BroadcastTextLocales_bak.sql | grep -E "${LOCALE}|/\*|SET CHARACTER|SET NAMES|RUNCATE TABLE|LOCK TABLES|UNLOCK TABLES" >>BroadcastTextLocales_temp.sql
+${MYSQL_COMMAND} <BroadcastTextLocales_temp.sql
+rm BroadcastTextLocales*.sql
 
 if [ ! -d translations_${CMANGOS_CORE} ]; then
   git clone https://github.com/MangosExtras/Mangos${CMANGOS_CORE}_Localised.git translations_${CMANGOS_CORE} -b master --recursive --depth=1 && cd translations_${CMANGOS_CORE}
 else
   cd translations_${CMANGOS_CORE} && git pull
 fi
-
-export MYSQL_PWD="${PASSWORD}"
-MYSQL_COMMAND="${MYSQL} -h${DB_HOST} -P${DB_PORT} -u${USERNAME} ${DATABASE}"
 
 echo "> Processing 1_LocaleTablePrepare.sql,2_Add_NewLocalisationFields.sql,3_InitialSaveEnglish.sql."
 if [ -f 1+2+3.sql ]; then
