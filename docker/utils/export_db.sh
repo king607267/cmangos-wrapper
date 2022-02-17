b3ee0b14bc6aa36a73febf8198b7bbc38e227b21
wangjueming <wangjueming@ule.com>
2022-02-17 14:33:04 +0800

    add realmd,characters db utils,install tzdata

---
+++ docker/utils/export_db.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#only export characters realmd
+function exportDB() {
+  cd
+  path="dataBack/$1"
+  if [ ! -d "$path" ]; then
+    /bin/mkdir -p $path && cd $path
+  else
+    cd $path
+  fi
+  day=$(date '+%Y-%m-%d')
+  if [ ! -d "$day" ]; then
+    /bin/mkdir -p $day && cd $day
+  else
+    cd $day
+  fi
+  fileName=""
+  if [ "$1" = "60" ]; then
+    fileName="WoWDBback60_$(date '+%H%M').sql"
+    #change username,pwd,host,port
+    mysqldump --no-tablespaces -t -umangos -pmangos -P3360 -h127.0.0.1 --databases classiccharacters classicrealmd --ignore-table=classicrealmd.antispam_blacklist --ignore-table=classicrealmd.antispam_replacement --ignore-table=classicrealmd.antispam_unicode_replacement --ignore-table=classicrealmd.realmlist >"$fileName"
+  elif [ "$1" = "70" ]; then
+    fileName="WoWDBback70_$(date '+%H%M').sql"
+    #change username,pwd,host,port
+    mysqldump --no-tablespaces -t -umangos -pmangos -P3370 -h127.0.0.1 --databases tbccharacters tbcrealmd --ignore-table=tbcrealmd.antispam_blacklist --ignore-table=tbcrealmd.antispam_replacement --ignore-table=tbcrealmd.antispam_unicode_replacement --ignore-table=tbcrealmd.realmlist >"$fileName"
+  elif [ "$1" = "80" ]; then
+    fileName="WoWDBback80_$(date '+%H%M').sql"
+    #change username,pwd,host,port
+    mysqldump --no-tablespaces -t -umangos -pmangos -P3380 -h127.0.0.1 --databases wotlkcharacters --ignore-table=wotlkcharacters.playerbot_talentspec wotlkrealmd --ignore-table=wotlkrealmd.realmlist --ignore-table=wotlkrealmd.antispam_blacklist --ignore-table=wotlkrealmd.antispam_replacement --ignore-table=wotlkrealmd.antispam_unicode_replacement >"$fileName"
+  fi
+}
+exportDB '60'
+#exportDB '70'
+#exportDB '80'
---
+++ docker/utils/import_db.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eo pipefail
+
+function importDB() {
+  #path see export_db.sh
+  cd
+  cd "dataBack/${1}/$(date '+%Y-%m-%d')"
+  #get newest file name
+  fileName=$(ls -t | head -1)
+  #delete ADMINISTRATOR,GAMEMASTER,MODERATOR,PLAYER
+  sed -i "s/INSERT INTO \`account\` VALUES /delete from \`account\` where username in ('ADMINISTRATOR','GAMEMASTER','MODERATOR','PLAYER');\nINSERT INTO \`account\` VALUES /" "$fileName"
+  MYSQL_COMMAND="${2} -h${3} -P${4} -p${5} -u${6}"
+  echo "use "%{fileName}
+  ${MYSQL_COMMAND} <"$fileName"
+}
+#change username,pwd,host,port
+importDB '60' 'mysql' "127.0.0.1" "3360" "mangos" "mangos"
+echo "import classic successful"
+#importDB '70' 'mysql' "127.0.0.1" "3370" "mangos" "mangos"
+#echo "import tbc successful"
+#importDB '80' 'mysql' "127.0.0.1" "3380" "mangos" "mangos"
+#echo "import wotlk successful"
